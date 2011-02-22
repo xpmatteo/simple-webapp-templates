bf24c98e935905048d3236d841b522b019a52c54
xpmatteo <vaccari@pobox.com>
2011-02-22 18:35:05 +0100

    cleanup even after a failing test

--- simple-webapp-template-winstone-ant-eclipse/script/integration-test.sh
+++ simple-webapp-template-winstone-ant-eclipse/script/integration-test.sh
@@ -8,6 +8,14 @@ function server_pid() {
   ps aux | grep -F -- '-jar lib/winstone-' | grep -v -F 'grep' | awk '{ print $2 }'
 }
 
+function shutdown() {
+  while [ "$(server_pid)" != "" ]; do
+    # echo kill $(server_pid)
+    kill $(server_pid)
+    sleep 1
+  done  
+}
+
 
 which lynx > /dev/null || fail "Install lynx"
 
@@ -18,12 +26,8 @@ ant war || exit 1
 
 echo "Starting server"
 script/server.sh > /dev/null &
+trap shutdown EXIT
 sleep 2
 
 lynx -dump http://localhost:8080/ | grep -qi 'Hello, World!' && echo "OK!" || fail "Didn't work"
 
-while [ "$(server_pid)" != "" ]; do
-  # echo kill $(server_pid)
-  kill $(server_pid)
-  sleep 1
-done
--- simple-webapp-template-winstone-buildr/script/integration-test.sh
+++ simple-webapp-template-winstone-buildr/script/integration-test.sh
@@ -8,6 +8,13 @@ function server_pid() {
   ps aux | grep -F -- '-jar lib/winstone-' | grep -v -F 'grep' | awk '{ print $2 }'
 }
 
+function shutdown() {
+  while [ "$(server_pid)" != "" ]; do
+    # echo kill $(server_pid)
+    kill $(server_pid)
+    sleep 1
+  done  
+}
 
 which lynx > /dev/null || fail "Install lynx"
 
@@ -18,12 +25,8 @@ buildr -s clean package || exit 1
 
 echo "Starting server"
 script/server.sh > /dev/null &
+trap shutdown EXIT
 sleep 2
 
-lynx -dump http://localhost:8080/ | grep -qi 'Hello, World!' && echo "OK!" || echo "Didn't work"
+lynx -dump http://localhost:8080/ | grep -qi 'Hello, World!' && echo "OK!" || fail "Didn't work"
 
-while [ "$(server_pid)" != "" ]; do
-  # echo kill $(server_pid)
-  kill $(server_pid)
-  sleep 1
-done
